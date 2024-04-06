89ca4560b6a5646dbe97668f3251d480b53f87dd
Miguel Willians <136731765+miguel-willians@users.noreply.github.com>
2024-04-06 12:45:11 -0300

    Update exercicio.js

--- Aula de Objetos/exercicio.js
+++ Aula de Objetos/exercicio.js
@@ -1,10 +1,18 @@
-// Exercício: IMPLEMENTE UMA FUNÇÃO QUE RECEBA UMA LISTA DE ALUNS (NOME, RA , NOTAP1, NOTAP2). Essa função deverá:
-//A) Mostrar na tela os alunos. medias.push(result); V
+// Exercício: IMPLEMENTE UMA FUNÇÃO QUE RECEBA UMA LISTA DE ALUNOS (NOME, RA , NOTAP1, NOTAP2). Essa função deverá:
+//A) Mostrar na tela os alunos.
 //B) Notas menores que 6 em vermelho, maiores ou iguais em azul.
-//C) Média P1 da TURMA V
-//D) Média P2 da TURMA V
-//E) A média cada aluno V
-//F) A quantidad de aprovdos e reprovados
+//C) Média P1 da turma. 
+//D) Média P2 da turma.
+//E) A média cada aluno.
+//F) A quantidad de aprovdos e reprovados.
+
+// Exercise: IMPLEMENT A FUNCTION THAT RECEIVES A LIST OF STUDENTS (NAME, ID (RA), GRADE1, GRADE2). This function should:
+// A) Display the students on the screen.
+// B) Grades less than 6 in red, greater or equal in blue.
+// C) Class average of Grade 1 (Média P1).
+// D) Class average of Grade 2 (Média P2).
+// E) Each student's average grade (Média).
+// F) The number of students approved and failed (Aprovados/Reprovados).
 
 var listaAlunos = [{"nome":"Miguel", "ra":"001","p1": 10, "p2" : 10},
                     {"nome":"Carlos", "ra":"002","p1": 10, "p2" : 8},
@@ -26,20 +34,16 @@ function tabelaAlunos(listaAlunos){
                 "</tr>"+
                 "</thead>"+
                 "<tbody>";
-    for (let i = 0; i < listaAlunos.length; i++){
-        tab = tab + "<tr><td>" + listaAlunos[i].nome + "</td>"+
-        "<td>" + listaAlunos[i].ra + "</td>" +
-        '<td id="score1">' + listaAlunos[i].p1 +"</td>" +
-        "<td>" + listaAlunos[i].p2 + "</td>" +
-        "<td>" + mediaInd[i] + "</td>" +
-        "<td>" + status[i] + "</td></tr>";
-        // if (listaAlunos[i].p1 <= 6){
-        //     let sc = document.getElementById("score1").style.color = "blue";
-        // }
-        // else{
-        //     let sc = document.getElementById("score1").style.color = "red";
-        // }
-    }
+  for (let i = 0; i < listaAlunos.length; i++){
+          let colorp1 = (listaAlunos[i].p1 <= 6) ? "blue" : "red";
+          let colorp2 = (listaAlunos[i].p1 <= 6) ? "blue" : "red";
+          tab = tab + "<tr><td>" + listaAlunos[i].nome + "</td>"+
+          "<td>" + listaAlunos[i].ra + "</td>" +
+          '<td class="score" style="color:' + colorp1 + ';">' + listaAlunos[i].p1 +"</td>" +
+          '<td class="score" style="color:' + colorp2 + ';">' + listaAlunos[i].p2 + "</td>" +
+          "<td>" + mediaInd[i] + "</td>" +
+          "<td>" + status[i] + "</td></tr>";
+      }
     tab = tab + "</tbody></table>";
 
     tabMedia = "<table><thead><tr><th>Média P1</th>" + "<th>Média P2</th>" + "<th>Aprovados</th>" + "<th>Reprovados</th>" + "</tr></thead>" +
